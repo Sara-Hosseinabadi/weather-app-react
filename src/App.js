97bcafa4cdc224e9087410b27ac4b4e2076d06d2
Sara-Hosseinabadi <121258586+Sara-Hosseinabadi@users.noreply.github.com>
2023-05-08 21:38:17 +0430

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1> weather app</h1>
-      <footer>
-        This project was coded by Sara Hosseinabadi and is{" "}
-        <a
-          href="https://github.com/Sara-Hosseinabadi/weather-app-react"
-          target="_blank"
-        >
-          {" "}
-          Open-sourced on GitHub
-        </a>
-      </footer>
+    <div className="App ">
+      <div className="container">
+        <h1> weather app</h1>
+
+        <footer>
+          This project was coded by Sara Hosseinabadi and is{" "}
+          <a
+            href="https://github.com/Sara-Hosseinabadi/weather-app-react"
+            target="_blank"
+          >
+            {" "}
+            Open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
